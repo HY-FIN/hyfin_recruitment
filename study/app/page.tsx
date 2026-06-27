88edc3ae16ac20e6e75892b014e6a2b63f6ad4c9
Babogaeguri <psyun0323@gmail.com>
2026-06-27 15:02:27 +0900

    add study profile page

---
+++ study/app/page.tsx
@@ -0,0 +1,21 @@
+export default function ProfilePage() {
+  return (
+    <div className="min-h-screen bg-gray-50 flex items-center justify-center">
+        <div className="bg-white rounded-2xl shadow-md p-10 w-full max-w-md">
+
+        <h1 className="text-3xl font-bold text-gray-900">박상윤</h1>
+        <p className="text-gray-500 mt-1">한양대학교 / 경영학과</p>
+
+        <p className="mt-6 text-gray-700 leading-relaxed">
+          안녕하세요! 저는 한양대학교 경영학과에 재학 중인 박상윤입니다. 다양한 분야에 관심이 많으며, 특히 기술과 비즈니스의 융합에 큰 흥미를 가지고 있습니다. 새로운 도전을 즐기며, 팀워크와 협업을 통해 더 나은 결과를 만들어가는 것을 중요하게 생각합니다. 앞으로도 지속적으로 성장하며, 다양한 경험을 쌓아 나가고자 합니다.
+        </p>
+
+        <div className="bg-white rounded-2xl shadow-md p-10 w-full max-w-md mt-8">
+            <p className="mt-6 text-gray-700 leading-relaxed">
+            저는 바보개구리에요. 우헤헤.
+            </p>
+        </div>
+        </div>
+    </div>
+  );
+}
